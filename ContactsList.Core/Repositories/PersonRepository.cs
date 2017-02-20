911f67c6601a180dbee4892a784d2faa2e4dbb4b
kkrzysiek <kszklarzewicz@gmail.com>
2017-02-20 21:31:28 +0100

    Added PersonRepository and PersonService

---
+++ ContactsList.Core/Repositories/PersonRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace ContactsList.Core
+{
+	public class PersonRepository: IPersonRepository
+	{
+		public IEnumerable<Person> GetPersons()
+		{
+			return persons;
+		}
+
+		public void Update(Person person)
+		{
+			persons.Remove(persons.First(x => x.Id == person.Id));
+			persons.Add(person);
+			persons = persons.OrderBy(x => x.Id).ToList();
+		}
+
+		public void SavePerson(Person person)
+		{
+			persons.Add(person);
+		}
+
+		private List<Person> persons = new List<Person>
+		{
+			new Person{ Id = 1, FirstName = "k", ImageName = "kkrzysiek", LastName = "krzysiek" },
+			new Person{ Id = 2, FirstName = "test", ImageName = "testtest", LastName = "test" },
+			new Person{ Id = 3, FirstName = "test1", ImageName = "test1test", LastName = "test1test" }
+		};
+	}
+}
---
+++ ContactsList.Core/Services/PersonService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace ContactsList.Core
+{
+	public class PersonService: IPersonService
+	{
+		private readonly IPersonRepository personRepository;
+
+		public PersonService(IPersonRepository personRepository)
+		{
+			this.personRepository = personRepository;
+		}
+
+		public List<Person> GetAllPersons()
+		{
+			var persons = personRepository.GetPersons().ToList();
+			if (persons == null || persons.Count() <= 0)
+				return new List<Person>();
+
+			return persons;
+		}
+
+		public void SavePerson(Person person)
+		{
+			if (string.IsNullOrEmpty(person.FirstName) || string.IsNullOrEmpty(person.LastName))
+				return;
+
+			// new person
+			if (person.Id == 0)
+			{
+				int highestId = personRepository.GetPersons().OrderByDescending(x => x.Id).First().Id;
+				person.Id = highestId + 1;
+				personRepository.SavePerson(person);
+				return;
+			}
+
+			// existing record
+			var p = personRepository.GetPersons().First(x => x.Id == person.Id);
+			if (p != null)
+			{
+				personRepository.Update(person);
+				return;
+			}
+		}
+
+		public Person GetById(int id)
+		{
+			var person = personRepository.GetPersons().FirstOrDefault(x => x.Id == id);
+			if (person == null)
+				return new Person();
+			return person;
+		}
+
+
+		public Person GetByEmail(string email)
+		{
+			var person = personRepository.GetPersons().FirstOrDefault(x => x.ImageName.ToLower() == email.ToLower());
+			if (person == null)
+				return new Person();
+			return person;
+		}
+	}
+
+}
