384421d490de914bb1cab79b136df71c34ba84e1
kkrzysiek <kszklarzewicz@gmail.com>
2017-02-20 21:39:00 +0100

    Add App setup

---
+++ ContactsList.Core/App.cs
@@ -0,0 +1,24 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.IoC;
+namespace ContactsList.Core
+{
+	public class App: MvxApplication
+	{
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			CreatableTypes()
+				.EndingWith("Repository")
+				.AsInterfaces()
+				.RegisterAsLazySingleton();
+
+			CreatableTypes()
+				.EndingWith("Service")
+				.AsInterfaces()
+				.RegisterAsLazySingleton();
+
+			RegisterAppStart(new AppStart());
+		}
+	}
+}
---
+++ ContactsList.Core/AppStart.cs
@@ -0,0 +1,12 @@
+﻿
+using MvvmCross.Core.ViewModels;
+namespace ContactsList.Core
+{
+	public class AppStart: MvxNavigatingObject, IMvxAppStart
+	{
+		public void Start(object hint = null)
+		{
+			ShowViewModel<PersonListViewModel>();
+		}
+	}
+}
