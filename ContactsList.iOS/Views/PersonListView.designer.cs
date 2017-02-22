f9beca681b7783065b35a9f31826250867bebc49
kkrzysiek <kszklarzewicz@gmail.com>
2017-02-22 18:46:48 +0100

    view cleanup

--- ContactsList.iOS/Views/PersonListView.designer.cs
+++ ContactsList.iOS/Views/PersonListView.designer.cs
@@ -1,4 +1,4 @@
-﻿// WARNING
+// WARNING
 //
 // This file has been generated automatically by Xamarin Studio from the outlets and
 // actions declared in your storyboard file.
@@ -11,11 +11,19 @@ using UIKit;
 
 namespace ContactsList.iOS
 {
-	[Register("PersonListView")]
-	partial class PersonListView
-	{
-		void ReleaseDesignerOutlets()
-		{
-		}
-	}
+    [Register ("PersonListView")]
+    partial class PersonListView
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITableView savedPersonTableView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (savedPersonTableView != null) {
+                savedPersonTableView.Dispose ();
+                savedPersonTableView = null;
+            }
+        }
+    }
 }
